e8667c2c7e8f725136468086856e62ae05cd0136
xingyu <xingyu.develop.cai@gmail.com>
2019-05-09 14:59:54 +0000

    add results

--- anomaly_detection/script_vae.sh
+++ anomaly_detection/script_vae.sh
@@ -8,6 +8,7 @@ do
   echo $filename
   mkdir saved_models/$filename
   mkdir saved_models/$filename/vae
+	mkdir saved_results/$filename/vae
   python3 main_vae.py iot_data/freezer_data/processed/$filename.TRAIN iot_data/freezer_data/processed/$filename.TEST --check_path saved_models/$filename/vae_h5.pk --results_path saved_results/$filename/vae/
   # python3 main_vae.py iot_data/freezer_data/processed/$filename.TRAIN iot_data/freezer_data/processed/$filename.TEST --load_check saved_models/$filename/vae_h5.pk
 done
